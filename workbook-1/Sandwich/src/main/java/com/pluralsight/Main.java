063497edb26296d158d0db6550bfded48585fb03
julian gonzalez <juliang4209@gmail.com>
2024-04-13 16:53:28 -0700

    demo fix

---
+++ workbook-1/Sandwich/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Main
+{
+    private static Scanner userInput = new Scanner(System.in);
+
+    {
+
+        String sandwichSize;
+        String orderNumber;
+
+
+
+
+        System.out.println("SandwichShop Online Order ");
+        System.out.println("(1)Regular - 5.45 | (2)Large - 8.95\n");
+        System.out.println();
+
+
+    }
+
+
+}
