9aeb5aa058dcf1ebc0eb26745b028c141c29f1f3
julian gonzalez <juliang4209@gmail.com>
2024-04-15 15:26:27 -0700

    demo 1 finished

--- workbook-2/FullNameGenerator/src/main/java/com/pluralsight/Main.java
+++ workbook-2/FullNameGenerator/src/main/java/com/pluralsight/Main.java
@@ -1,13 +1,63 @@
 package com.pluralsight;
 
+
 import java.util.Scanner;
 
 public class Main
 {
-    Scanner
+    public static  Scanner userInput = new Scanner(System.in);
 
     public static void main(String[] args)
     {
+        String firstName = firstName();
+        String middleName = middleName();
+        String lastName = lastName();
+        String suffix = suffix();
+        fullName(firstName, middleName,lastName,suffix);
+
 
     }
+    public static String firstName()
+    {
+        System.out.print("First Name:");
+        String firstName = userInput.nextLine().strip();
+        return firstName;
+
+    }
+    public static String middleName()
+    {
+        System.out.print("Middle Name:");
+        String middleName = userInput.nextLine().strip();
+        if (!middleName.isEmpty())
+        {
+            middleName += ".";
+        }
+        return middleName;
+    }
+    public static String lastName()
+    {
+        System.out.print("last Name:");
+        String lastName = userInput.nextLine().strip();
+        return lastName;
+    }
+    public static String suffix()
+    {
+        System.out.print("Suffix:");
+        String suffix = userInput.nextLine().strip();
+
+        return suffix;
+    }
+    public static void fullName(String firstName, String middleName, String lastname, String suffix)
+    {
+        if(!suffix.isEmpty())
+        {
+            lastname += ",";
+        }
+
+        String fullName = "Full Name:" + firstName + " " + middleName + " " + lastname + " " +suffix;
+        fullName = fullName.replace("  "," ");
+        System.out.println(fullName);
+    }
+
+
 }
---
+++ workbook-2/HighScoreWins/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static Scanner userInput = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        // Home:Visitor|21:9
+
+    }
+}
