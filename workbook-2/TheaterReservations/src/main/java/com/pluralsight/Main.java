5a5423c72b099f9b171817a768c19018f946f76e
julian gonzalez <juliang4209@gmail.com>
2024-04-16 15:59:47 -0700

    More Work on demo 4

--- workbook-2/TheaterReservations/src/main/java/com/pluralsight/Main.java
+++ workbook-2/TheaterReservations/src/main/java/com/pluralsight/Main.java
@@ -1,10 +1,56 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main
 {
+    public static Scanner userInput = new Scanner(System.in);
+
     public static void main(String[] args)
     {
-        int 
+
+
+        int tickets;
+
+       System.out.print("Number Of tickets:");
+       tickets = userInput.nextInt();
+       userInput.nextLine();
+
+       displayReservation(tickets);
+        getName();
+
+
 
     }
+
+    public static void displayReservation(int tickets)
+    {
+        if (tickets > 1)
+        {
+            System.out.println(tickets+ " Tickets");
+        }
+        else
+        {
+            System.out.println(tickets + " Ticket");
+        }
+
+    }
+    public static String  getName()
+    {
+        String firstName;
+        String lastName;
+        String name;
+
+
+        System.out.println("Please Enter Full Name");
+        name = userInput.nextLine();
+        String[] parts = name.split(" ");
+        firstName = parts[0].strip();
+        lastName = parts[1].strip();
+        return lastName + "," + firstName;
+
+
+    }
+
+
 }
