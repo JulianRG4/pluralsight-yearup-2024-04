64ea583ef111c105e8905a3ef02e4a407b0303be
julian gonzalez <juliang4209@gmail.com>
2024-05-08 13:24:18 -0700

    demo work

--- workbook-4/demos/02-overloading-demo-starter/src/main/java/com/pluralsight/Main.java
+++ workbook-4/demos/02-overloading-demo-starter/src/main/java/com/pluralsight/Main.java
@@ -1,5 +1,18 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Main
 {
+    public static void main(String[] args)
+    {
+        GuestHistory history = new GuestHistory();
+
+        ArrayList<Reservation> reservations = history.getAllReservations();
+
+        for (Reservation reservation : reservations)
+        {
+            System.out.println(reservation.getRoom().getRoomNumber() + ": " + reservation.);
+        }
+    }
 }
