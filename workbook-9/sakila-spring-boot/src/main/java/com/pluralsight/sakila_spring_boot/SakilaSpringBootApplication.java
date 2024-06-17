80fe651b4aa3cfe10dc4f71bd98ff61faefb1bd7
julian gonzalez <juliang4209@gmail.com>
2024-06-17 14:45:40 -0700

    work along

---
+++ workbook-9/sakila-spring-boot/src/main/java/com/pluralsight/sakila_spring_boot/SakilaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.sakila_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakilaSpringBootApplication.class, args);
+	}
+
+}
